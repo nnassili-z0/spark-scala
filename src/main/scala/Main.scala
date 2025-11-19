1c2710671eb50425b5129c59cbd57b5295e6240b
nnassili <naoufal.nassili@alten.com>
2025-11-19 15:16:32 +0100

    chore: push latest changes to dev and main (2025-11-19 15:15 local)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package ma.naoufanassili.sparkscalastuff
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,13 +14,24 @@ object Main {
       .config("spark.driver.bindAddress", "127.0.0.1")
       .getOrCreate()
 
-    val df = spark.read
+    val df: DataFrame = spark.read
       .option("header", value = true)
       .option("inferSchema", value = true )
       .csv("data/AAPL.csv")
 
     df.show()
     df.printSchema()
+
+    df.select("Date", "Open", "Close").show()
+
+    // col("Date")
+    import spark.implicits._
+    // $"Date"
+
+    df.select(col("Date"), $"Open", df("close")).show()
+
+    val column = df("Date")
+    df.select(column, $"Open", df("close")).show()
   }
 }
 
